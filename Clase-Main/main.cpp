7f84d3cc0690015a30f69b65488837243cf3bc76
JohanEchavarria <johan_ech@hotmail.com>
2025-05-14 12:24:10 -0400

    Add files via upload

---
+++ Clase-Main/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //indica la/*comentario en varias lineas*/
+    // comentario en una sola linea
+
+    //tipos de datos
+    //cout indica el flujo de salida
+    //endl que la linea termino
+    //<< indica el flujo de salida
+    //>> indica el flujo de Entrada
+    // cin indica el flujo de entrada de
+    //tipos de datos
+    // Datos Emtero
+    // suma de dos numero entero
+    /*int num1, num2, suma;
+    cout << "ingrese el primer numero entero:";
+    cin >> num1;
+    cout << "ingrese el segundo numero entero:";
+    cin >> num2;
+    suma = num1 + num2;
+    cout << "la suma es: " << suma << endl;*/
+
+
+    //datos tipo float
+    //promedio de temperaturas
+    /* float t1,t2.t3, promedio;
+
+    cout << "ingrese tres temperatura (float):";
+    cin >> t1 >> t2 >> t3;
+
+    promedio= (t1+t2+t3)/3;
+    cout << "Promedio de temperatura: " << promedio <<   "°C" << endl;*/
+    //Tipo de Datos double
+    //Tipo double - calculo de IMC
+
+    /*double peso, altura;
+    cout << "ingrese tu peso (kg):";
+    cin >> peso;
+    cout << "ingrese su Altura (m): ";
+    cin >> altura;
+    double IMC=peso/ pow (altura,2);
+    cout << "Tu IMC es:"<<IMC<< endl;*/
+
+    //Tipo Char
+    //Tipo char -- Identificar si es vocal
+
+   /* char letra;
+    cout << "ingrese una letra:";
+    cin>> letra;
+    if (letra == 'a' || letra== 'e' || letra=='i' || letra=='o' || letra=='u'||
+        letra == 'A' || letra== 'E' || letra=='I' || letra=='O' || letra=='U'){
+            cout << "Es una vocal."<<endl;
+
+        }else{
+        cout << "no es una vocal."<<endl;
+
+        }*/
+
+     // Tipo bool - Verificacion de mayoria de Edad
+
+     /*int edad;
+     bool esMayor;
+     cout <<"ingresa tu edad: ";
+     cin >> edad;
+
+     esMayor=(edad>=18);
+     cout <<"Eres Mayor de edad?: "<< (esMayor ? "Si":"No")  <<endl;*/
+
+     /*string nombre;
+     cout << "Como te Lamas?: ";
+     getline(cin, nombre);
+
+     cout << "Hola," <<nombre << "!bienvenido a C++." <<endl;*/
+
+
+
+
+    return 0;
+
+
+}
